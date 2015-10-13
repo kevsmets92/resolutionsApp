9dceffe7690cadc555f19b7d875b903b0ba8e50d
Kevin Smets <kevin.smets@student.kuleuven.be>
2015-10-13 03:50:22 +0200

    initial commit: mongodb explored + changes from events/helpers

---
+++ resolutions.js
@@ -0,0 +1,41 @@
+Resolutions = new Mongo.Collection('resolutions');
+
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    resolutions: function(){
+      //get all resolutions from mongo collection
+      return Resolutions.find();
+    } 
+  });
+
+  Template.body.events({
+    'submit .new-resolution': function(event){
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        title: title,
+        createdAt: new Date()
+      });
+      event.target.title.value="";
+      return false;
+    }
+  });
+
+  Template.resolution.events({
+    'click .toggle-checked': function(){
+      Resolutions.update(this._id,{
+        $set: {checked: !this.checked}
+      });
+    },
+    'click .delete': function(){
+      Resolutions.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
